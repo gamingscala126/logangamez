3198ad61a0f336e97136015fb1aec9a7482d14b6
gamingscala126 <johansanker@gmail.com>
2024-05-15 09:08:05 -0400

    ggg

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ var Player = function(id){
 	}
     self.collision = function(block)
     {
-        self.maxSpdX += 2*((self.x)-(block.x))*Math.random();
-        self.maxSpdY += 2*((self.y)-(block.y))*Math.random();
+        self.maxSpdX += ((self.x)-(block.x))*Math.random();
+        self.maxSpdY += ((self.y)-(block.y))*Math.random();
     }
 	return self;
 }
